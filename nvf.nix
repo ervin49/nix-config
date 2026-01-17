a2e07528b38805a2b1c74a70a86331763304a6c3
ervin49 <ervinmarin49@gmail.com>
2026-01-18 01:35:41 +0200

    Rebuild: nixl - 2026-01-18 01:35

--- nvf.nix
+++ nvf.nix
@@ -1,9 +1,31 @@
-# nvf.nix
-{ pkgs, inputs, system, ... }:
+{ pkgs, ... }:
 
 {
-    programs.nvf = {
-        enable = true;
-    };
-}
+  programs.nvf = {
+    enable = true;
 
+    settings = {
+      # 1. Everything must be inside the 'vim' block
+      vim = {
+        theme = {
+          enable = true;
+          name = "dracula";
+          style = "dark";
+        };
+
+        # 2. Separate treesitter from LSP
+        treesitter = {
+          enable = true;
+        };
+
+        lsp = {
+          enable = true;
+        };
+
+        # 3. I have removed extraPlugins because enabling lsp and treesitter 
+        # above AUTOMATICALLY installs these plugins for you.
+        # If you add them manually here, it will cause conflicts.
+      };
+    };
+  };
+}
