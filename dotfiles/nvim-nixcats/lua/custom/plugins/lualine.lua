5d128bc19453f71eecceaee1d14e1656729cd7a5
ervin49 <ervinmarin49@gmail.com>
2026-01-18 23:10:36 +0200

    Update: nixl - 2026-01-18 23:10

--- dotfiles/nvim-nixcats/lua/custom/plugins/lualine.lua
+++ dotfiles/nvim-nixcats/lua/custom/plugins/lualine.lua
@@ -1,29 +1,16 @@
--- Mai intai, o verificare de siguranta (idiomul standard in Nix/Lua config)
--- Daca pluginul nu e instalat prin flake.nix, sa nu dea eroare tot editorul.
 local status_ok, lualine = pcall(require, "lualine")
 if not status_ok then
 	return
 end
 
--- Aici incepe configuratia directa, fara "config = function()"
 lualine.setup {
     options = {
         icons_enabled = true,
         theme = 'auto',
-        component_separators = { left = '', right = ''},
-        section_separators = { left = '', right = ''},
-        disabled_filetypes = {
-            statusline = {},
-            winbar = {},
-        },
-        ignore_focus = {},
+        component_separators = '',
+        section_separators = '',
         always_divide_middle = true,
         globalstatus = false,
-        refresh = {
-            statusline = 1000,
-            tabline = 1000,
-            winbar = 1000,
-        }
     },
     sections = {
         lualine_a = {'mode'},
@@ -33,16 +20,4 @@ lualine.setup {
         lualine_y = {'progress'},
         lualine_z = {'location'}
     },
-    inactive_sections = {
-        lualine_a = {},
-        lualine_b = {},
-        lualine_c = {'filename'},
-        lualine_x = {'location'},
-        lualine_y = {},
-        lualine_z = {}
-    },
-    tabline = {},
-    winbar = {},
-    inactive_winbar = {},
-    extensions = {}
 }
