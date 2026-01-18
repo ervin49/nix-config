9d041369fe08a3cbf8a9d5951db66d3d9d6f1856
ervin49 <ervinmarin49@gmail.com>
2026-01-18 21:49:39 +0200

    Rebuild: nixl - 2026-01-18 21:49

--- dotfiles/nvim-nixcats/lua/custom/plugins/lualine.lua
+++ dotfiles/nvim-nixcats/lua/custom/plugins/lualine.lua
@@ -1,45 +1,48 @@
-return {
-    'lualine.nvim',
-    config = function()
-    require('lualine').setup {
-        options = {
-            icons_enabled = true,
-            theme = 'auto',
-            component_separators = { left = '', right = ''},
-            section_separators = { left = '', right = ''},
-            disabled_filetypes = {
-                statusline = {},
-                winbar = {},
-            },
-            ignore_focus = {},
-            always_divide_middle = true,
-            globalstatus = false,
-            refresh = {
-                statusline = 1000,
-                tabline = 1000,
-                winbar = 1000,
-            }
-        },
-        sections = {
-            lualine_a = {'mode'},
-            lualine_b = {'diff', 'diagnostics'},
-            lualine_c = {'filename'},
-            lualine_x = {'fileformat', 'filetype'},
-            lualine_y = {'progress'},
-            lualine_z = {'location'}
-        },
-        inactive_sections = {
-            lualine_a = {},
-            lualine_b = {},
-            lualine_c = {'filename'},
-            lualine_x = {'location'},
-            lualine_y = {},
-            lualine_z = {}
-        },
-        tabline = {},
-        winbar = {},
-        inactive_winbar = {},
-        extensions = {}
-    }
+-- Mai intai, o verificare de siguranta (idiomul standard in Nix/Lua config)
+-- Daca pluginul nu e instalat prin flake.nix, sa nu dea eroare tot editorul.
+local status_ok, lualine = pcall(require, "lualine")
+if not status_ok then
+	return
 end
+
+-- Aici incepe configuratia directa, fara "config = function()"
+lualine.setup {
+    options = {
+        icons_enabled = true,
+        theme = 'auto',
+        component_separators = { left = '', right = ''},
+        section_separators = { left = '', right = ''},
+        disabled_filetypes = {
+            statusline = {},
+            winbar = {},
+        },
+        ignore_focus = {},
+        always_divide_middle = true,
+        globalstatus = false,
+        refresh = {
+            statusline = 1000,
+            tabline = 1000,
+            winbar = 1000,
+        }
+    },
+    sections = {
+        lualine_a = {'mode'},
+        lualine_b = {'diff', 'diagnostics'},
+        lualine_c = {'filename'},
+        lualine_x = {'fileformat', 'filetype'},
+        lualine_y = {'progress'},
+        lualine_z = {'location'}
+    },
+    inactive_sections = {
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = {'filename'},
+        lualine_x = {'location'},
+        lualine_y = {},
+        lualine_z = {}
+    },
+    tabline = {},
+    winbar = {},
+    inactive_winbar = {},
+    extensions = {}
 }
