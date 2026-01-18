b31e72c6cd859cac0add1b6f80d1a51579fefa58
ervin49 <ervinmarin49@gmail.com>
2026-01-18 23:20:22 +0200

    Update: nixl - 2026-01-18 23:20

--- dotfiles/nvim-nixcats/lua/custom/plugins/lualine.lua
+++ dotfiles/nvim-nixcats/lua/custom/plugins/lualine.lua
@@ -6,7 +6,7 @@ end
 lualine.setup {
     options = {
         icons_enabled = true,
-        theme = 'auto',
+        theme = 'iceberg_dark',
         component_separators = '',
         section_separators = '',
         always_divide_middle = true,
