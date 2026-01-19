1050777858a5b8274802d98078eff9c8fe8b6b14
ervin49 <ervinmarin49@gmail.com>
2026-01-19 19:29:12 +0200

    Rebuild: nixl - 2026-01-19 19:29

--- laptop/configuration.nix
+++ laptop/configuration.nix
@@ -1,15 +1,29 @@
-{config, pkgs, lib, ...}:
+{ config, pkgs, lib, ... }:
 
 {
     imports =
         [ 
-            ./hardware-configuration.nix
+        ./hardware-configuration.nix
             ../common.nix
         ];
 
     networking.hostName = "nixl";
-    services.tlp.enable = true;
+
     services.power-profiles-daemon.enable = lib.mkForce false;
 
+    services.tlp = {
+        enable = true;
+
+        settings = {
+            CPU_SCALING_GOVERNOR_ON_AC = "performance";
+            CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+
+            CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+            CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+
+            START_CHARGE_THRESH_BAT0 = 20;
+            STOP_CHARGE_THRESH_BAT0 = 80;
+        };
+    };
     system.stateVersion = "25.05"; 
 }
