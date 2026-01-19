bfbb6422b2202759102a5071d6d11198e6315d5c
ervin49 <ervinmarin49@gmail.com>
2026-01-20 00:08:38 +0200

    Update: nixl - 2026-01-20 00:08

--- dotfiles/nvim-nixcats/lua/custom/plugins/lsp.lua
+++ dotfiles/nvim-nixcats/lua/custom/plugins/lsp.lua
@@ -2,18 +2,16 @@
 local blink_status, blink = pcall(require, "blink.cmp")
 local capabilities = blink_status and blink.get_lsp_capabilities() or {}
 
--- NOTA: Nu mai folosim require('lspconfig') aici, folosim API-ul nativ vim.lsp
-
 ---------------------------------------
 -- 1. Configurare CLANGD (C/C++)
 ---------------------------------------
--- Definim configuratia
 vim.lsp.config('clangd', {
-    cmd = { 'clangd' },
-filetypes = { "h", "c", "cpp", "objc", "objcpp" },
-root_markers = { '.git', 'compile_commands.json', 'compile_flags.txt' },
-capabilities = capabilities,
-})
+    cmd = { 'clangd' }, -- Am inchis acolada aici. Nu lasa virgula in interior daca e un singur element, e mai safe.
+    filetypes = { "h", "c", "cpp", "objc", "objcpp" },
+    root_markers = { '.git', 'compile_commands.json', 'compile_flags.txt' },
+    capabilities = capabilities,
+}) 
+-- NOTA: La finalul functiei trebuie '})' -> o acolada pentru tabela, o paranteza pentru functie.
 
 -- Activam serverul
 vim.lsp.enable('clangd')
@@ -22,10 +20,10 @@ vim.lsp.enable('clangd')
 -- 2. Configurare JDTLS (Java)
 ---------------------------------------
 vim.lsp.config('jdtls', {
-    cmd = { 'jdtls' },
-    -- root_markers inlocuieste root_dir in noua versiune
-    root_markers = { '.git', 'mvnw', 'gradlew', 'pom.xml' }, 
-    capabilities = capabilities,
+    cmd = { 'jdtls' },
+    -- root_markers inlocuieste root_dir in noua versiune
+    root_markers = { '.git', 'mvnw', 'gradlew', 'pom.xml' }, 
+    capabilities = capabilities,
 })
 
 -- Activam serverul
@@ -35,12 +33,12 @@ vim.lsp.enable('jdtls')
 -- 3. Shortcut-uri (Keymaps)
 ---------------------------------------
 vim.api.nvim_create_autocmd("LspAttach", {
-    callback = function(event)
-        local opts = { buffer = event.buf }
+    callback = function(event)
+        local opts = { buffer = event.buf }
 
-        vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-        vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-        vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
-        vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
-    end,
+        vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+        vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+        vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+        vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+    end,
 })
