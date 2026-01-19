721850cb7309f52dbcd987fb7fd485ba3d134fbe
ervin49 <ervinmarin49@gmail.com>
2026-01-20 01:34:29 +0200

    Update: nixl - 2026-01-20 01:34

--- dotfiles/nvim-nixcats/lua/custom/plugins/harpoon.lua
+++ dotfiles/nvim-nixcats/lua/custom/plugins/harpoon.lua
@@ -26,10 +26,10 @@ vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end, { desc = "
 vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end, { desc = "Harpoon: Toggle UI" })
 
 -- Navigare rapidă (fișierele 1-4)
-vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
-vim.keymap.set("n", "<C-t>", function() harpoon:list():select(2) end)
-vim.keymap.set("n", "<C-n>", function() harpoon:list():select(3) end)
-vim.keymap.set("n", "<C-s>", function() harpoon:list():select(4) end)
+vim.keymap.set("n", "<M-j>", function() harpoon:list():select(1) end)
+vim.keymap.set("n", "<M-k>", function() harpoon:list():select(2) end)
+vim.keymap.set("n", "<M-l>", function() harpoon:list():select(3) end)
+vim.keymap.set("n", "<M-;>", function() harpoon:list():select(4) end)
 
 -- Opțional: Navigare previous/next
 vim.keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end)
