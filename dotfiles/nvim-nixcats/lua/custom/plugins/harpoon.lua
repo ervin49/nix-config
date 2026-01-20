9234867cefdf5e301e268ec46358f3c0f51839fb
ervin49 <ervinmarin49@gmail.com>
2026-01-20 18:03:40 +0200

    Update: nixl - 2026-01-20 18:03

--- dotfiles/nvim-nixcats/lua/custom/plugins/harpoon.lua
+++ dotfiles/nvim-nixcats/lua/custom/plugins/harpoon.lua
@@ -1,21 +1,31 @@
+-- Fisier: custom/plugins/harpoon.lua
 return {
-    "harpoon2", -- Numele plugin-ului din flake.nix (probabil harpoon2 sau harpoon)
-    -- Plugin-ul se încarcă DOAR când apeși una dintre aceste taste
-    load = function()
+    -- 1. Numele pachetului din Nix. 
+    -- Daca in flake.nix ai pkgs.vimPlugins.harpoon2, lasa "harpoon2".
+    "harpoon2", 
+
+    -- 2. ACESTA ESTE FIX-UL: Spunem lui lze ca atunci cand codul cere modulul "harpoon",
+    -- el trebuie sa activeze pachetul "harpoon2" din Nix.
+    on_require = { "harpoon" },
+
+    -- 3. Triggere pentru incarcare: Plugin-ul porneste cand apesi aceste taste.
+    keys = {
+        { "<leader>a", function() require("harpoon"):list():add() end, desc = "Harpoon Add" },
+        { "<C-e>", function() require("harpoon").ui:toggle_quick_menu(require("harpoon"):list()) end, desc = "Harpoon Menu" },
+        { "<C-j>", function() require("harpoon"):list():select(1) end, desc = "Harpoon Select 1" },
+        { "<C-k>", function() require("harpoon"):list():select(2) end, desc = "Harpoon Select 2" },
+        { "<C-l>", function() require("harpoon"):list():select(3) end, desc = "Harpoon Select 3" },
+        { "<C-h>", function() require("harpoon"):list():select(4) end, desc = "Harpoon Select 4" },
+    },
+
+    -- 4. Configurarea propriu-zisa: Se executa DOAR dupa ce pachetul e incarcat.
+    after = function()
         local harpoon = require("harpoon")
-        
-        harpoon:setup()
-
-        -- Keybinds clasice
-        vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end, { desc = "Harpoon Add" })
-
-        -- Meniul nativ Harpoon
-        vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end, { desc = "Harpoon Menu" })
-
-        -- Navigare rapida
-        vim.keymap.set("n", "<C-j>", function() harpoon:list():select(1) end)
-        vim.keymap.set("n", "<C-k>", function() harpoon:list():select(2) end)
-        vim.keymap.set("n", "<C-l>", function() harpoon:list():select(3) end)
-        vim.keymap.set("n", "<C-;>", function() harpoon:list():select(4) end)
+        harpoon:setup({
+            settings = {
+                save_on_toggle = true,
+                sync_on_ui_close = true,
+            },
+        })
     end,
 }
