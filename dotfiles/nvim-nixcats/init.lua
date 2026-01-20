5c64e14fc529ff745d9db29e729a5e4f31c6c33a
ervin49 <ervin49@gmail.com>
2026-01-21 00:55:37 +0200

    Update: nixp - 2026-01-21 00:55

--- dotfiles/nvim-nixcats/init.lua
+++ dotfiles/nvim-nixcats/init.lua
@@ -1,8 +1,3 @@
--- 1. Setup-ul de bază pentru nixCats (obligatoriu pentru a avea comanda nixCats)
-require('nixCatsUtils').setup({
-  non_nix_value = true,
-})
-
 -- 2. Setări de bază (Leader key trebuie setat ÎNAINTE de lazy)
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
