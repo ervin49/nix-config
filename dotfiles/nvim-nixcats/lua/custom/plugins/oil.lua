90cad7d98840500eee8ca08b24de8a26efef05b1
ervin49 <ervinmarin49@gmail.com>
2026-01-20 16:39:21 +0200

    Update: nixl - 2026-01-20 16:39

--- dotfiles/nvim-nixcats/lua/custom/plugins/oil.lua
+++ dotfiles/nvim-nixcats/lua/custom/plugins/oil.lua
@@ -1,8 +1,5 @@
--- Nu mai este nevoie de pcall(require, "oil") aici, 
--- deoarece 'lze' se ocupă de gestionarea erorilor și a încărcării.
-
 return {
-    "oil-nvim", -- Numele plugin-ului (cum e definit în nixCats)
+    "oil.nvim", -- Numele plugin-ului (cum e definit în nixCats)
     on_cmd = "Oil", -- Se încarcă doar când rulezi comanda :Oil
     -- on_ft = "directory", -- Sau când deschizi un director
     load = function()
