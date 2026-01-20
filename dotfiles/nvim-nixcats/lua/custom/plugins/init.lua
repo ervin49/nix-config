9127d2189b5ecc321c12a6d8b5e07c73dec32199
ervin49 <ervinmarin49@gmail.com>
2026-01-20 16:32:02 +0200

    Update: nixl - 2026-01-20 16:32

--- dotfiles/nvim-nixcats/lua/custom/plugins/init.lua
+++ dotfiles/nvim-nixcats/lua/custom/plugins/init.lua
@@ -1,11 +1,11 @@
-require('lze').load( 
+require('lze').load({
     { import = "custom.plugins.lualine" },
     { import = "custom.plugins.harpoon" },
     { import = "custom.plugins.themery" },
     { import = "custom.plugins.oil" },
     { import = "custom.plugins.blink" },
-    { import = "custom.plugins.diagnostics"},
-)
+    { import = "custom.plugins.diagnostics"}
+})
 require("custom.plugins.snacks")
 require("custom.plugins.transparent")
 require("custom.plugins.lsp")
