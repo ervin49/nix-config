c8f60ca918fc662a8cc63a0504b7755e1e4eaca0
ervin49 <ervin49@gmail.com>
2026-01-21 23:03:16 +0200

    Update: nixp - 2026-01-21 23:03

--- dotfiles/nvim-nixcats/lua/custom/plugins/diagnostics.lua
+++ dotfiles/nvim-nixcats/lua/custom/plugins/diagnostics.lua
@@ -1,49 +1,34 @@
-return {
-  {
-    "rachartier/tiny-inline-diagnostic.nvim",
-    
-    -- 1. Verificare NixCats
-    enabled = true,
-    
-    -- 2. Încărcare: "VeryLazy" sau la evenimentul "LspAttach" e cel mai sigur
-    event = "VeryLazy", 
-    priority = 1000, -- Vrem să se încarce printre primele de UI
+{
+  "rachartier/tiny-inline-diagnostic.nvim",
+  event = { "BufReadPost", "BufNewFile" },
+  priority = 1000,
+  config = function()
+      require('tiny-inline-diagnostic').setup({
+          preset = "powerline", 
+          hi = {
+              error = "DiagnosticError",
+              warn = "DiagnosticWarn",
+              info = "DiagnosticInfo",
+              hint = "DiagnosticHint",
+          },
+          options = {
+              show_source = true, 
+              use_icons_from_diagnostic = true,
+              add_messages = true,
+              pills = false,
+              blend = { factor = 0.2 },
+              enable_on_insert = false,
+              virt_texts = { priority = 2048 },
+          },
+      })
 
-    -- 3. Configurare
-    config = function()
-        -- Aici lipim exact codul tău de setup
-        require('tiny-inline-diagnostic').setup({
-            preset = "powerline", 
-            hi = {
-                error = "DiagnosticError",
-                warn = "DiagnosticWarn",
-                info = "DiagnosticInfo",
-                hint = "DiagnosticHint",
-            },
-            options = {
-                show_source = true, 
-                use_icons_from_diagnostic = true,
-                add_messages = true,
-                pills = false,
-                blend = {
-                    factor = 0.2,
-                },
-                enable_on_insert = false,
-                virt_texts = {
-                    priority = 2048,
-                },
-            },
-        })
-
-        -- 4. Tot aici punem și logica de dezactivare a textului standard
-        -- E important să fie aici ca să se execute DOAR când se încarcă pluginul
-        vim.diagnostic.config({
-            virtual_text = false, -- Oprim textul standard ca să nu se suprapună cu Tiny
-            signs = true,
-            underline = true,
-            update_in_insert = false,
-            severity_sort = true,
-        })
-    end
-  }
+      vim.diagnostic.config({
+          virtual_text = false,
+          signs = true,
+          underline = true,
+          update_in_insert = false,
+          severity_sort = true,
+      })
+  end
 }
+
