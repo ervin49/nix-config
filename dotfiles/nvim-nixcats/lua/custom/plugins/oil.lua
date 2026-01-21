477f6701d073855a6234bf92ca64a7b666da365e
ervin49 <ervin49@gmail.com>
2026-01-21 22:59:28 +0200

    Update: nixp - 2026-01-21 22:59

--- dotfiles/nvim-nixcats/lua/custom/plugins/oil.lua
+++ dotfiles/nvim-nixcats/lua/custom/plugins/oil.lua
@@ -1,10 +1,6 @@
 return {
   {
     "stevearc/oil.nvim",
-    cmd = "Oil",
-    keys = {
-      { "-", "<cmd>Oil<cr>", desc = "Open parent directory" },
-    },
     dependencies = { "nvim-tree/nvim-web-devicons" },
     config = function()
       require("oil").setup({
@@ -13,11 +9,6 @@ return {
         view_options = { show_hidden = true },
         skip_confirm_for_simple_edits = true,
       })
-
-      -- Dacă Neovim a fost lansat cu un director (ex: nvim .), deschide Oil automat
-      if vim.fn.isdirectory(vim.fn.expand("%:p")) == 1 then
-        vim.cmd.Oil()
-      end
     end
   }
 }
