be507e65da572cf333d0c057b695dd0056cdd197
ervin49 <ervin49@gmail.com>
2026-01-22 22:32:46 +0200

    Update: nixp - 2026-01-22 22:32

--- dotfiles/nvim-nixcats/lua/custom/plugins/harpoon.lua
+++ dotfiles/nvim-nixcats/lua/custom/plugins/harpoon.lua
@@ -1,19 +1,28 @@
 return {
-  {
-    "ThePrimeagen/harpoon",
-    branch = "harpoon2",
-    dependencies = { "nvim-lua/plenary.nvim" },
-    keys = {
-      { "<leader>a", function() require("harpoon").list():add() end, desc = "Harpoon: Add file" },
-      { "<C-e>", function() require("harpoon").ui:toggle_quick_menu(require("harpoon").list()) end, desc = "Harpoon: Toggle UI" },
-      { "<M-j>", function() require("harpoon").list():select(1) end },
-      { "<M-k>", function() require("harpoon").list():select(2) end },
-      { "<M-l>", function() require("harpoon").list():select(3) end },
-      { "<M-;>", function() require("harpoon").list():select(4) end },
-    },
-    config = function()
-        require("harpoon"):setup()
-    end,
-  }
-}
+    {
+        "ThePrimeagen/harpoon",
+        lazy = false,
+        config = function()
+            local harpoon = require("harpoon")
 
+            -- REQUIRED
+            harpoon:setup()
+            -- REQUIRED
+
+            local ui = require("harpoon.ui")
+            local mark = require("harpoon.mark")
+
+            -- :: Adaugă fișier în Harpoon
+            vim.keymap.set("n", "<leader>a", function() mark:add_file() end, { desc = "Add file to Harpoon" })
+
+            -- :: Deschide meniul rapid Harpoon
+            vim.keymap.set("n", "<C-e>", function() ui:toggle_quick_menu() end, { desc = "Toggle Harpoon Menu" })
+
+            -- :: Salt direct la fișierele Harpoon
+            vim.keymap.set("n", "<C-j>", function() ui:nav_file(1) end, { desc = "Harpoon File 1" })
+            vim.keymap.set("n", "<C-k>", function() ui:nav_file(2) end, { desc = "Harpoon File 2" })
+            vim.keymap.set("n", "<C-l>", function() ui:nav_file(3) end, { desc = "Harpoon File 3" })
+            vim.keymap.set("n", "<C-;>", function() ui:nav_file(4) end, { desc = "Harpoon File 4" })
+        end,
+    }
+}
