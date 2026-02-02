39bc472f7671e7ce7de1ad2a44ef3b958f733325
ervin49 <ervin49@gmail.com>
2026-02-02 18:28:25 +0200

    Update: nixp - 2026-02-02 18:28

--- common.nix
+++ common.nix
@@ -121,6 +121,22 @@
             settings = {
                 main = {
                     capslock = "overload(control, escape)";
+                    space = "overload(nav, space)";
+                };
+                nav = {
+                    h = "left";
+                    j = "down";
+                    k = "up";
+                    l = "right";
+
+                    u = "backspace";
+                    i = "delete";
+
+                    y = "home";
+                    o = "end";
+
+                    n = "pagedown";
+                    p = "pageup";
                 };
             };
         };
